74717053e5555f8194b7e246ddbe519d3b51ab5a
gloria <cmxgloria@gmail.com>
2020-07-01 19:37:36 +1000

    install react-router-dom updated props

--- src/component/homepage/HomePage.jsx
+++ src/component/homepage/HomePage.jsx
@@ -1,55 +1,55 @@
-import React from "react";
+// import React from "react";
 
-import "./homepage.styles.scss";
+// import "./homepage.styles.scss";
 
-const HomePage = () => (
-  <div className="homepage">
-    <h1>GLORIA'S ART AND CRAFT</h1>
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">MILLITARY&PARIS ART</div>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">KEYRING</div>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title"> CHILDREN PURSE</div>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">LADY PURSE</div>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">HANDBAG</div>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">JEWELLERY</div>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <div className="title">SALE</div>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+// const HomePage = () => (
+//   <div className="homepage">
+//     <h1>GLORIA'S ART AND CRAFT</h1>
+//     <div className="directory-menu">
+//       <div className="menu-item">
+//         <div className="content">
+//           <div className="title">MILLITARY&PARIS ART</div>
+//           <span className="subtitle">SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className="menu-item">
+//         <div className="content">
+//           <div className="title">KEYRING</div>
+//           <span className="subtitle">SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className="menu-item">
+//         <div className="content">
+//           <div className="title"> CHILDREN PURSE</div>
+//           <span className="subtitle">SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className="menu-item">
+//         <div className="content">
+//           <div className="title">LADY PURSE</div>
+//           <span className="subtitle">SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className="menu-item">
+//         <div className="content">
+//           <div className="title">HANDBAG</div>
+//           <span className="subtitle">SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className="menu-item">
+//         <div className="content">
+//           <div className="title">JEWELLERY</div>
+//           <span className="subtitle">SHOP NOW</span>
+//         </div>
+//       </div>
+//       <div className="menu-item">
+//         <div className="content">
+//           <div className="title">SALE</div>
+//           <span className="subtitle">SHOP NOW</span>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// );
 
-export default HomePage;
+// export default HomePage;
