8c9d7ab2d4c0b67bc197f45ce62e66160f5a1c92
gloria <cmxgloria@gmail.com>
2020-07-01 19:52:24 +1000

    fixed the router in index.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
 
 import HomePage from "./pages/homepage/HomePage.jsx";
 
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <HomePage />
-      </div>
-    );
-  }
+const HandmadeArt = () => (
+  <div>
+    <h1>HATMADE ART </h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/handmade-art" component={HandmadeArt} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
