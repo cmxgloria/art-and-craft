d075809de1008786e7add09989c9e7c6b33fd1fa
gloria <cmxgloria@gmail.com>
2020-07-01 18:31:05 +1000

    created homepage

---
+++ src/component/homepage/HomePage.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import "./homepage.style.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <h1>GLORIA'S ART AND CRAFT</h1>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">MILLITARY&PARIS ART</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">KEYRING</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title"> CHILDREN PURSE</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">LADY PURSE</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">HANDBAG</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">JEWELLERY</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <div className="title">SALE</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
