2f8b94120a3e44f99f8f88fb39fc5bf2176bc2bc
gloria <cmxgloria@gmail.com>
2020-07-02 22:19:03 +1000

    updated css

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Header from "../../component/header/Header";
 import Directory from "../../component/directory/Directory";
 
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
+    <Header />
     <Directory />
   </div>
 );
