97bdfe948d2ea89b7bc785f7030780e6eff18bea
gloria <cmxgloria@gmail.com>
2020-07-03 10:53:40 +1000

    created signin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Contact from "./pages/contact/Contact.jsx";
 import HomePage from "./pages/homepage/HomePage.jsx";
 import ShopPage from "./pages/shop/ShopPage.jsx";
+import SignIn from "./component/sign-in/SignIn.jsx";
 
 import "./App.css";
 
@@ -19,6 +20,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/contact" component={Contact} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignIn} />
         <Route path="/handmade-art" component={HandmadeArt} />
       </Switch>
     </div>
