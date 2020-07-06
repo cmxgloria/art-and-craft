a1b3d01034fbb7695ce814f1b8aa74c756aa05af
gloria <cmxgloria@gmail.com>
2020-07-06 15:35:08 +1000

    created SignUp.jsx and scss

--- src/pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx
+++ src/pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../component/sign-in/SignIn";
+import SignUp from "../../component/sign-up/SignUp";
 
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
