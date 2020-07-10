d40ce6d76af2ac74b381f252791a6cc67ab5eda5
gloria <cmxgloria@gmail.com>
2020-07-10 12:33:48 +1000

    updated header.jsx and app.js how to use connect HOC with react-redux

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils.js";
-
+import { connect } from "react-redux";
 import "./header.styles.scss";
 
 const Header = ({ currentUser }) => (
@@ -31,5 +31,7 @@ const Header = ({ currentUser }) => (
     </div>
   </div>
 );
-
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+export default connect(mapStateToProps)(Header);
