07306519758b2f920904e07564ac0284c04f91f1
gloria <cmxgloria@gmail.com>
2020-07-11 20:11:59 +1000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import Contact from "./pages/contact/Contact.jsx";
 import HomePage from "./pages/homepage/HomePage.jsx";
 import ShopPage from "./pages/shop/ShopPage.jsx";
@@ -7,7 +8,7 @@ import Header from "./component/header/Header.jsx";
 // import SignIn from "./component/sign-in/SignIn.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/SignInAndSignUpPage.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-
+import { setCurrentUser } from "./redux/user/user.actions";
 import "./App.css";
 
 const HandmadeArt = () => (
@@ -28,21 +29,37 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    //   if (userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
+
+    //     userRef.onSnapshot((snapShot) => {
+    //       this.setState({
+    //         currentUser: {
+    //           id: snapShot.id,
+    //           ...snapShot.data(),
+    //         },
+    //       });
+    //     });
+    //   }
+
+    //   this.setState({ currentUser: userAuth });
+    // });
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
         });
       }
 
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -66,4 +83,7 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+export default connect(null, mapDispatchToProps)(App);
