0025e5bf3137bda635620700f77c9a328d21b61c
gloria <cmxgloria@gmail.com>
2020-07-15 14:47:57 +1000

    updated shop data

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -6,60 +6,59 @@ const SHOP_DATA = [
     items: [
       {
         id: 1,
-        name: "large metal tank",
+        name: "A1-large metal tank(dark grey available)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2018/261/352/9384253162_2022656010.400x400.jpg",
         price: 25,
       },
       {
         id: 2,
-        name: "bullet small tank&plane(big$25)",
+        name: "A2bullet small tank&plane(big$25)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2015/523/844/2581448325_2022656010.400x400.jpg",
         price: 15,
       },
       {
         id: 3,
-        name: "small paris-tower(big$15)",
+        name: "A3small paris-tower with diamond 8 colors(big$15)",
         imageUrl:
-          "https://cbu01.alicdn.com/img/ibank/2015/103/449/2273944301_2022656010.400x400.jpg",
+          "https://cbu01.alicdn.com/img/ibank/2017/809/211/4310112908_170595664.400x400.jpg",
         price: 10,
       },
       {
         id: 4,
-        name: "motor-bike",
-        imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
-        price: 25,
+        name: "A4motor-bike(copper&dark grey-differnt design)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2014/650/967/1518769056_2022656010.400x400.jpg",
+        price: 15,
       },
       {
         id: 5,
-        name: "Green Beanie",
-        imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
-        price: 18,
+        name: "A5big bullet tank(2 design with-wheel or without-wheel)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2018/788/037/9490730887_2022656010.400x400.jpg",
+        price: 25,
       },
       {
         id: 6,
-        name: "Palm Tree Cap",
-        imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
-        price: 14,
+        name: "A6large bullet plane",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2014/757/004/1807400757_2022656010.400x400.jpg",
+        price: 20,
       },
       {
         id: 7,
-        name: "Red Beanie",
-        imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
-        price: 18,
+        name: "A7small bullet tank",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2018/017/275/9409572710_2022656010.400x400.jpg",
+        price: 15,
       },
       {
         id: 8,
-        name: "Wolf Cap",
-        imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
-        price: 14,
-      },
-      {
-        id: 9,
-        name: "Blue Snapback",
-        imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
-        price: 16,
+        name: "A8new design metal tank",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/388/500/4018005883_2022656010.400x400.jpg",
+        price: 25,
       },
     ],
   },
@@ -69,54 +68,60 @@ const SHOP_DATA = [
     routeName: "keyring",
     items: [
       {
-        id: 10,
-        name: "Adidas NMD",
-        imageUrl: "https://i.ibb.co/0s3pdnc/adidas-nmd.png",
-        price: 220,
+        id: 9,
+        name: "K1handmade cystal(80 designs-small,medium,large) ",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2012/778/274/608472877_935227936.400x400.jpg",
+        price: 5,
       },
       {
-        id: 11,
-        name: "rabbit keyring",
+        id: 10,
+        name: "K2rabbit keyring",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2017/862/877/4065778268_43763277.400x400.jpg",
         price: 10,
       },
       {
-        id: 12,
-        name: "ball keyring(10 colours)",
+        id: 11,
+        name: "K3ball keyring(10 colours)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2017/055/605/4048506550_43763277.400x400.jpg",
         price: 5,
       },
       {
+        id: 12,
+        name: "K4handmade crystal keyring-medium(50 design)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2012/647/484/665484746_935227936.400x400.jpg",
+        price: 6,
+      },
+      {
         id: 13,
-        name: "Nike White AirForce",
-        imageUrl: "https://i.ibb.co/1RcFPk0/white-nike-high-tops.png",
-        price: 160,
+        name: "K5handmade crystal keyring-medium(50 design)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2013/664/902/1086209466_935227936.400x400.jpg",
+        price: 6,
       },
       {
         id: 14,
-        name: "Nike Red High Tops",
-        imageUrl: "https://i.ibb.co/QcvzydB/nikes-red.png",
-        price: 160,
+        name: "K6pokemon keyring(50 designs)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/170/382/3995283071_374272229.400x400.jpg",
+        price: 4,
       },
       {
         id: 15,
-        name: "Nike Brown High Tops",
-        imageUrl: "https://i.ibb.co/fMTV342/nike-brown.png",
-        price: 160,
+        name: "K7handmadde rose keyring(80 designs-buy3get1free)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2013/906/605/761506609_935227936.400x400.jpg",
+        price: 5,
       },
       {
         id: 16,
-        name: "Air Jordan Limited",
-        imageUrl: "https://i.ibb.co/w4k6Ws9/nike-funky.png",
-        price: 190,
-      },
-      {
-        id: 17,
-        name: "Timberlands",
-        imageUrl: "https://i.ibb.co/Mhh6wBg/timberlands.png",
-        price: 200,
+        name: "K8large kitty(30 design-buy3get1free)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2012/969/615/667516969_935227936.400x400.jpg",
+        price: 7,
       },
     ],
   },
@@ -126,38 +131,39 @@ const SHOP_DATA = [
     routeName: "children-purse",
     items: [
       {
-        id: 18,
-        name: "3 cat purse(long&short-blue,navy,black,pink)",
+        id: 17,
+        name: "C1three cat purse(long&short-blue,navy,black,pink)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2018/459/113/9418311954_301242762.400x400.jpg",
         price: 15,
       },
       {
-        id: 19,
-        name: "cartoon boy purse(40 designs)",
+        id: 18,
+        name: "C2cartoon boy purse(40 designs)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2016/667/179/3498971766_1758631755.400x400.jpg",
         price: 10,
       },
       {
-        id: 20,
-        name: "cat&dog coin purse(30 designs)",
+        id: 19,
+        name: "C3cat&dog coin purse(30 designs)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2018/043/973/8856379340_822287278.400x400.jpg",
         price: 6,
       },
       {
-        id: 21,
-        name: "Unicorn purse(10 designs:frozen,jack,kitty...)",
+        id: 20,
+        name: "C4Unicorn purse(long&short-10 designs:frozen,jack,kitty...)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2019/427/533/10926335724_1872069914.400x400.jpg",
         price: 10,
       },
       {
-        id: 22,
-        name: "Tan Trench",
-        imageUrl: "https://i.ibb.co/M6hHc3F/brown-trench.png",
-        price: 185,
+        id: 21,
+        name: "C5elephant purse(6 designs)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2019/462/984/10884489264_1872069914.400x400.jpg",
+        price: 10,
       },
     ],
   },
@@ -167,51 +173,69 @@ const SHOP_DATA = [
     routeName: "lady-purse",
     items: [
       {
-        id: 23,
-        name: "fancy flower purse(black,blue,khaki,pink)",
+        id: 22,
+        name: "L1fancy flower purse(black,blue,khaki,pink)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2018/055/537/9398735550_1424842467.400x400.jpg",
         price: 15,
       },
       {
-        id: 24,
-        name: "Totoro(short-black,purple,orange,green)",
+        id: 23,
+        name: "L2Totoro(short-black,purple,orange,green)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2018/240/363/9454363042_301242762.400x400.jpg",
         price: 15,
       },
       {
-        id: 25,
-        name: "fancy cat purse(purple,hotpink,blue,black)",
+        id: 24,
+        name: "L3fancy cat purse(purple,hotpink,blue,black)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2015/569/025/2639520965_1424842467.400x400.jpg",
         price: 15,
       },
       {
-        id: 26,
-        name: "handmade purse",
+        id: 25,
+        name: "L4handmade purse",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2016/240/216/3101612042_645572075.400x400.jpg",
-        price: 80,
+        price: 10,
       },
       {
-        id: 27,
-        name: "Totoro(Long-black,purple,orange,green)",
+        id: 26,
+        name: "L5Totoro(Long-black,purple,orange,green)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2018/358/913/9463319853_301242762.400x400.jpg",
         price: 18,
       },
       {
-        id: 28,
-        name: "Yellow Track Suit",
-        imageUrl: "https://i.ibb.co/v1cvwNf/yellow-track-suit.png",
-        price: 135,
+        id: 27,
+        name: "L6leaf purse(Long&short:red,purple,black,khaki)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2018/525/741/8706147525_1324055139.400x400.jpg",
+        price: 15,
       },
       {
+        id: 28,
+        name: "L7cute flower purse(blue,pink,green,purple)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2018/147/086/8735680741_1324055139.400x400.jpg",
+        price: 15,
+      },
+
+      {
         id: 29,
-        name: "White Blouse",
-        imageUrl: "https://i.ibb.co/qBcrsJg/white-vest.png",
-        price: 20,
+        name: "L8cute flower purse(blue,pink,green,purple)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/640/927/7869729046_156013192.400x400.jpg",
+        price: 18,
+      },
+
+      {
+        id: 30,
+        name: "L9cute flower purse(blue,pink,green,purple)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/197/840/6784048791_1324055139.400x400.jpg",
+        price: 18,
       },
     ],
   },
@@ -221,77 +245,86 @@ const SHOP_DATA = [
     routeName: "handbag",
     items: [
       {
-        id: 30,
-        name: "Lipstick-Logo(blue,black,pink,grey)",
+        id: 31,
+        name: "B1Lipstick-Logo(blue,black,pink,grey)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2017/703/814/4381418307_27799352.400x400.jpg",
         price: 25,
       },
       {
-        id: 31,
-        name: "Elephant-Logo(black,green,white,pink)",
+        id: 32,
+        name: "B2Elephant-Logo(black,green,white,pink)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2018/729/319/9005913927_1511143656.400x400.jpg",
         price: 25,
       },
       {
-        id: 32,
-        name: "bettle-logo(black,pink&red)",
+        id: 33,
+        name: "B3bettle-logo(black,pink&red)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2019/956/874/11185478659_27799352.400x400.jpg",
         price: 25,
       },
       {
-        id: 33,
-        name: "hello-kitty handbag(white,lightpink,hotpink,gold)",
+        id: 34,
+        name: "B4hello-kitty handbag(white,lightpink,hotpink,gold)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2017/041/247/7771742140_1460651700.400x400.jpg",
         price: 15,
       },
 
       {
-        id: 34,
-        name: "Cool-middle-strip(pink&black)",
+        id: 35,
+        name: "B5Cool-middle-strip(pink&black)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2017/547/378/7735873745_27799352.400x400.jpg",
         price: 25,
       },
       {
-        id: 100,
-        name: "Sexy-mouth-logo(black&pink)",
+        id: 36,
+        name: "B6Sexy-mouth-logo(black&pink)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2017/397/207/7589702793_1511143656.400x400.jpg",
         price: 25,
       },
       {
-        id: 35,
-        name: "double-colour",
+        id: 37,
+        name: "B7double-colour",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2019/446/131/11249131644.80x80.jpg?_=2020",
         price: 20,
       },
       {
-        id: 36,
-        name: "gold-pin(black-white&white-red)",
+        id: 38,
+        name: "B8gold-pin(black-white&white-red)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2019/886/620/12671026688.80x80.jpg?_=2020",
         price: 25,
       },
 
       {
-        id: 37,
-        name: "classic angel(black&pink)",
+        id: 39,
+        name: "B9classic angel(black&pink)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2018/757/320/9636023757_1511143656.400x400.jpg",
         price: 20,
       },
 
       {
-        id: 38,
-        name: "cat&hot shoulder handbag(12 different designs)",
+        id: 40,
+        name: "B10cat&hot shoulder handbag(12 different designs)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2015/036/000/2161000630_822287278.400x400.jpg",
         price: 20,
+      },
+
+      {
+        id: 41,
+        name:
+          "B11handmade Thailand handbag with long strap(4 color-big available)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/098/001/4243100890_645572075.400x400.jpg",
+        price: 15,
       },
     ],
   },
@@ -301,63 +334,67 @@ const SHOP_DATA = [
     routeName: "jewellery",
     items: [
       {
-        id: 1,
-        name: "star-light(10 different colour)",
+        id: 42,
+        name: "J1star-light(10 different colour)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2017/484/876/4466678484_1984651542.400x400.jpg",
         price: 15,
       },
       {
-        id: 2,
-        name: "ocean(darkblue,lightblue,white)",
+        id: 43,
+        name: "J2handmade crystal(40 designs&colour)",
         imageUrl:
-          "https://cbu01.alicdn.com/img/ibank/2015/844/634/2178436448_1984651542.400x400.jpg",
+          "https://cbu01.alicdn.com/img/ibank/2017/395/125/4378521593_1844830307.400x400.jpg",
         price: 15,
       },
 
       {
-        id: 3,
-        name: "fancy sparkling design",
+        id: 44,
+        name: "J3fancy sparkling design",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2015/857/327/2479723758_1984651542.400x400.jpg",
         price: 15,
       },
       {
-        id: 4,
-        name: "cute casual loveheart(pink,blue,blue,white)",
+        id: 45,
+        name: "J4cute casual loveheart(pink,blue,blue,white)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2015/700/884/2224488007_1984651542.400x400.jpg",
         price: 15,
       },
       {
-        id: 5,
-        name: "Green Beanie",
-        imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
-        price: 18,
+        id: 46,
+        name: "J5handmade crystal with leaf pedant",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/827/718/7851817728_2045298408.400x400.jpg",
+        price: 15,
       },
       {
-        id: 6,
-        name: "with-loveheart(8 colours)",
+        id: 47,
+        name: "J6with-loveheart(8 colours)",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2017/416/943/4011349614_1984651542.400x400.jpg",
         price: 15,
       },
       {
-        id: 7,
-        name: "Red Beanie",
-        imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
-        price: 18,
+        id: 48,
+        name: "J7ocean(darkblue,lightblue,white)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2015/844/634/2178436448_1984651542.400x400.jpg",
+        price: 15,
       },
       {
-        id: 8,
-        name: "Wolf Cap",
-        imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
+        id: 49,
+        name: "J8cool blue ocean crystal",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/126/843/7852348621_2045298408.400x400.jpg",
         price: 14,
       },
       {
-        id: 9,
-        name: "Blue Snapback",
-        imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
+        id: 50,
+        name: "J9handmade crystal with ball",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/456/454/7838454654_2045298408.400x400.jpg",
         price: 16,
       },
     ],
@@ -368,60 +405,46 @@ const SHOP_DATA = [
     routeName: "sale",
     items: [
       {
-        id: 1,
-        name: "Australian&American-logo purse",
+        id: 51,
+        name: "S1Australian&American-logo purse",
         imageUrl:
           "https://cbu01.alicdn.com/img/ibank/2019/230/274/12692472032_2084417701.400x400.jpg",
         price: 10,
       },
       {
-        id: 2,
-        name: "tank",
+        id: 52,
+        name: "S2kitty purse",
         imageUrl:
-          "https://cbu01.alicdn.com/img/ibank/2018/800/997/9490799008_2022656010.400x400.jpg",
-        price: 15,
+          "https://cbu01.alicdn.com/img/ibank/2018/111/657/9565756111_176921582.400x400.jpg",
+        price: 7,
       },
       {
-        id: 3,
-        name: "paris-tower",
-        imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
-        price: 10,
-      },
-      {
-        id: 4,
-        name: "Grey Brim",
-        imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
-        price: 25,
-      },
-      {
-        id: 5,
-        name: "Green Beanie",
-        imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
+        id: 53,
+        name: "S3angle handbag",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2018/757/320/9636023757_1511143656.400x400.jpg",
         price: 18,
       },
       {
-        id: 6,
-        name: "Palm Tree Cap",
-        imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
-        price: 14,
+        id: 54,
+        name: "S4cat&dog shining purse",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2019/663/074/11184470366_1872069914.400x400.jpg",
+        price: 9,
       },
       {
-        id: 7,
-        name: "Red Beanie",
-        imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
+        id: 55,
+        name: "S5fancy middle-strip handbag(pink&black)",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2017/547/378/7735873745_27799352.400x400.jpg",
         price: 18,
       },
       {
-        id: 8,
-        name: "Wolf Cap",
-        imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
-        price: 14,
-      },
-      {
-        id: 9,
-        name: "Blue Snapback",
-        imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
-        price: 16,
+        id: 56,
+        name: "S6double-color handbag",
+        imageUrl:
+          "https://cbu01.alicdn.com/img/ibank/2019/446/131/11249131644.80x80.jpg?_=2020",
+        price: 18,
       },
     ],
   },
