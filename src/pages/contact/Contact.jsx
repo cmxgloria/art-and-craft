daf37ff20a2d594808f431a9d039ec4972be7d69
gloria <cmxgloria@gmail.com>
2020-07-22 21:33:41 +1000

    updated contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -7,6 +7,10 @@ export default class Contact extends React.Component {
       <div className="contact">
         <h1>Gloria</h1>
         <h2>Email: smile168@live.com.au</h2>
+        <h2>
+          Payment: Please Place Order in the Cart,Take Checkout Photo to Me then
+          I Organise Payment
+        </h2>
         <h2>Address: Elizabeth Street Victoria, 3000</h2>
         <h2>Available: More Design on Each Product and Welcome Your Enquiry</h2>
         <h3>Within Australia Post available</h3>
