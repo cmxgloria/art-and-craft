37570b7074e5a9dfa952496d2573b65b2de57a1c
gloria <cmxgloria@gmail.com>
2020-07-23 15:25:11 +1000

    updated shop.selector and fixed collectionOverview.jsx

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,15 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  handmadeart: 1,
-  keyring: 2,
-  childrenpurse: 3,
-  ladypurse: 4,
-  handbag: 5,
-  jewellery: 6,
-  sale: 7,
-};
+//  after "changed shop.data.js from array to object", no need id_map
+// const COLLECTION_ID_MAP = {
+//   handmadeart: 1,
+//   keyring: 2,
+//   childrenpurse: 3,
+//   ladypurse: 4,
+//   handbag: 5,
+//   jewellery: 6,
+//   sale: 7,
+// };
 
 const selectShop = (state) => state.shop;
 
@@ -18,8 +19,10 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    selectCollections,
+    (collections) => collections[collectionUrlParam]
+    // collections.find(
+    //   (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    // )
   );
