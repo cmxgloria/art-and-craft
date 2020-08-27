ca113b9cb70b22ec8acbb3fddf21c1ee02506ee9
gloria <cmxgloria@gmail.com>
2020-08-27 21:15:28 +1000

    Updates

---
+++ precache-manifest.bf82273b3d8d87c0c3f28ac8b9d204a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db7ac891f638b577247bb00c42ac6f7",
+    "url": "/art-and-craft/index.html"
+  },
+  {
+    "revision": "b2f9b8a3071c9884ea48",
+    "url": "/art-and-craft/static/css/main.0e51fd6e.chunk.css"
+  },
+  {
+    "revision": "529c37b490e83aec5166",
+    "url": "/art-and-craft/static/js/2.2c9b0e93.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/art-and-craft/static/js/2.2c9b0e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f9b8a3071c9884ea48",
+    "url": "/art-and-craft/static/js/main.44fd8d9f.chunk.js"
+  },
+  {
+    "revision": "2392670ad9943068c1ed",
+    "url": "/art-and-craft/static/js/runtime-main.00c9c4dd.js"
+  }
+]);
