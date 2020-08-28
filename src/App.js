d401f14a565bcc37d3f0021f7ca2e4c2fd2cba0a
gloria <cmxgloria@gmail.com>
2020-08-28 20:24:40 +1000

    updated app.js route

--- src/App.js
+++ src/App.js
@@ -80,10 +80,14 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          {/* <Route path="/art-and-craft" component={HomePage} /> */}
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/contact" component={Contact} />
-          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route path="/art-and-craft" component={HomePage} />
+          <Route path="/art-and-craft/shop" component={ShopPage} />
+          <Route path="/art-and-craft/contact" component={Contact} />
+          <Route
+            exact
+            path="/art-and-craft/checkout"
+            component={CheckoutPage}
+          />
           <Route
             exact
             path="/signin"
@@ -95,7 +99,7 @@ class App extends React.Component {
               )
             }
           />
-          <Route path="/handmade-art" component={HandmadeArt} />
+          <Route path="/art-and-craft/handmade-art" component={HandmadeArt} />
         </Switch>
       </div>
     );
