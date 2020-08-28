840696422726fe10c216b69e80e5529faa20501e
gloria <cmxgloria@gmail.com>
2020-08-28 20:32:19 +1000

    updated route

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends React.Component {
         <GlobalStyle />
         <Header />
         <Switch>
-          <Route path="/" component={HomePage} />
+          <Route exact path="/" component={HomePage} />
           <Route path="/art-and-craft" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route path="/contact" component={Contact} />
