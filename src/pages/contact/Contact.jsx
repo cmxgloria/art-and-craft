44a7e582f663da0363636a89654cad37faf51141
gloria <cmxgloria@gmail.com>
2021-05-26 11:20:46 +1000

    updated contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -7,21 +7,18 @@ export default class Contact extends React.Component {
       <div className="contact">
         <h2>Email: smilec2008@hotmail.com</h2>
         <h2>
-          I am a website designer as well. Happy to build your own awesome
-          website.
-        </h2>
-        <h3>
-          Please copy my website to browser to view. Sometimes it is not working
-          when you just click on the mobile because of "Surge" open source not
-          support.
-        </h3>
-        <h2>
           Payment: Please Place Order in the Cart,Take Checkout Photo to Me then
-          I Organise Payment
+          I Organise the rest.
         </h2>
-        <h2>Address: Elizabeth Street Victoria, 3000</h2>
-        <h2>Available: More Design on Each Product and Welcome Your Enquiry</h2>
-        <h3>Within Australia Post available</h3>
+        <h2>Address:Melbourne, 3000</h2>
+        <h2>
+          Available: More Design on Each Product and Welcome Your Enquiry.
+          Within Australia Post available.
+        </h2>
+        <h2>
+          ******I am also a website designer. Happy to build your own awesome
+          website.******
+        </h2>
       </div>
     );
   }
