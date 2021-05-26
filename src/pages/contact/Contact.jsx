8e7a43c6a5e4158f74671632e3fb29c329521524
gloria <cmxgloria@gmail.com>
2021-05-26 11:13:29 +1000

    updated contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import "./contact.styles.scss";
+import React from 'react';
+import './contact.styles.scss';
 
 export default class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <h1>Gloria</h1>
-        <h2>Email: smile168@live.com.au</h2>
+        <h2>Email: smilec2008@hotmail.com</h2>
         <h2>
           I am a website designer as well. Happy to build your own awesome
           website.
